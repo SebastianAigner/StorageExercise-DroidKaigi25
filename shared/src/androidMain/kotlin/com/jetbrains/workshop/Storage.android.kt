fe43ccdd00448db73a90964eaaa5aaf789563913
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-07-22 11:57:59 +0200

    Base project

---
+++ shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.workshop
+
+import android.app.Application
+import android.content.Context
+
+val context get() = ContextHelper.currentContext!!
---
+++ shared/src/commonMain/kotlin/com/jetbrains/workshop/Storage.kt
@@ -0,0 +1,14 @@
+package com.jetbrains.workshop
+
+fun saveString(key: String, value: String) {
+    println("Saving string: $key, $value")
+    // TODO: create and call expect/actual function:
+    // persistString(key, value)
+}
+
+fun loadString(key: String): String {
+    println("Loading string: $key")
+    // TODO: create and call expect/actual function:
+    // return restoreString(key)
+    return ""
+}
